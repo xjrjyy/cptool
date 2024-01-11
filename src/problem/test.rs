45a330f2b52a30987be581e3df13c5b97f6885eb
jrjyy <1479412322@qq.com>
2024-01-11 13:20:47 +0800

    refactor problem.rs

---
+++ src/problem/test.rs
@@ -0,0 +1,53 @@
+use crate::error::Result;
+use crate::program::Program;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub trait GetProgram {
+    fn get_program(&self, name: &str) -> Result<&Program>;
+}
+
+pub trait GetTestBundle {
+    fn get_test_bundle(&self, name: &str) -> Result<&TestBundle>;
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TestCase {
+    pub args: Vec<String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TestBundle {
+    // TODO: generator or raw input
+    #[serde(rename = "generator")]
+    pub generator_name: String,
+    pub cases: Vec<TestCase>,
+}
+
+impl TestBundle {
+    pub fn generate<T>(&self, programs: &T, inputs: Vec<std::fs::File>) -> Result<()>
+    where
+        T: GetProgram,
+    {
+        let generator = programs.get_program(&self.generator_name)?;
+        self.cases
+            .iter()
+            .zip(inputs.into_iter())
+            .map(|(case, input)| generator.run(case.args.clone(), None, input))
+            .collect::<Result<_>>()?;
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TestTask {
+    pub score: f64,
+    pub bundles: Vec<String>,
+    // TODO: task type
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Test {
+    pub bundles: HashMap<String, TestBundle>,
+    pub tasks: HashMap<String, TestTask>,
+}
