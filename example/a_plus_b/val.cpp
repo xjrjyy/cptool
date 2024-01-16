d98b5a2ec6e507cff7521b7abbe60e722c60b875
jrjyy <1479412322@qq.com>
2024-01-16 17:58:17 +0800

    update validator

--- example/a_plus_b/val.cpp
+++ example/a_plus_b/val.cpp
@@ -1,58 +1,15 @@
-#include <cctype>
-#include <iostream>
-#include <iterator>
-#include <string>
+#include "testlib.h"
 
 constexpr int V = 1e9;
 
-int main() {
-    std::cin >> std::noskipws;
+int main(int argc, char *argv[]) {
+    registerValidation(argc, argv);
 
-    std::istream_iterator<char> it(std::cin), end;
-    std::string s(it, end);
-
-    auto cur = s.begin();
-    auto peekChar = [&]() -> char {
-        if (cur == s.end()) {
-            std::exit(1);
-        }
-        return *cur;
-    };
-    auto consumeChar = [&]() {
-        ++cur;
-    };
-
-    auto readInt = [&](int low, int high) {
-        char c = peekChar();
-        if (!std::isdigit(c)) {
-            std::exit(1);
-        }
-        int result = 0;
-        while (std::isdigit(c)) {
-            consumeChar();
-            long long newResult = 1ll * result * 10 + c - '0';
-            if (newResult < low || newResult > high) {
-                std::exit(1);
-            }
-            result = newResult;
-            c = peekChar();
-        }
-        return result;
-    };
-
-    int a = readInt(1, V);
-    if (peekChar() != ' ') {
-        std::exit(1);
-    }
-    consumeChar();
-    int b = readInt(1, V);
-    if (peekChar() != '\n') {
-        std::exit(1);
-    }
-    consumeChar();
-    if (cur != s.end()) {
-        std::exit(1);
-    }
+    inf.readInt(1, V, "a");
+    inf.readSpace();
+    inf.readInt(1, V, "b");
+    inf.readEoln();
+    inf.readEof();
 
     return 0;
 }
